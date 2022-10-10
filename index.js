4da2620d0264118c959a0fd367634878ba18dcc0
angelica <angelica.cavero@gmail.com>
2022-10-10 12:10:55 +0200

    fetch pokemon list

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-console.log("hello");
-
-fetch("https://pokeapi.co/api/v2/pokemon/")
+const pokemons = fetch("https://pokeapi.co/api/v2/pokemon/")
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    const promises = data.results.map(async (pokeItem) => {
+      const response = await fetch(pokeItem.url);
+      const pokemon = await response.json();
+      return pokemon;
+    });
+    return Promise.all(promises);
+  })
+  .then((pokemonsListInfo) => {
+    showPokemonsList(pokemonsListInfo);
+  });
+
+showPokemonsList = (pokemons) => {
+  const pokemonsList = document.querySelector("#pokedex");
+  pokemons.forEach((pokemon) => {
+    const pokemonElement = document.createElement("li");
+    const pokemonBox = document.createElement("div");
+    const pokemonTitle = document.createElement("h2");
+    const pokemonImage = document.createElement("img");
+
+    pokemonImage.setAttribute("src", pokemon.sprites.front_default);
+    pokemonTitle.innerText = pokemon.name;
+    pokemonsList.append(pokemonElement);
+    pokemonElement.append(pokemonBox);
+    pokemonBox.append(pokemonTitle);
+    pokemonBox.append(pokemonImage);
+  });
+};
