f54d30594a05d7d866fccbac4d2f72bc02fb11f6
angelica <angelica.cavero@gmail.com>
2022-10-18 20:20:11 +0200

    types added

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const colours = [
+  {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD",
+  },
+];
+
 const container = document.querySelector(".container");
 const pokemonSearch = document.createElement("input");
 pokemonSearch.type = "search";
@@ -37,20 +60,38 @@ showPokemonsList = (pokemons) => {
   pokemonsList.innerHTML = "";
   pokemons.forEach((pokemon) => {
     const pokemonElement = document.createElement("li");
+    pokemonElement.className = "pokedex-box";
     const pokemonBox = document.createElement("div");
     const pokemonTitle = document.createElement("h2");
     const pokemonImage = document.createElement("img");
-    pokemonImage.setAttribute("src", pokemon.sprites.front_default);
+
+    pokemonImage.setAttribute(
+      "src",
+      pokemon.sprites.other.dream_world.front_default
+    );
+
     const imageToggle = () => {
-      pokemonImage.src === pokemon.sprites.front_default
+      pokemonImage.src === pokemon.sprites.other.dream_world.front_default
         ? (pokemonImage.src = pokemon.sprites.back_default)
-        : (pokemonImage.src = pokemon.sprites.front_default);
+        : (pokemonImage.src = pokemon.sprites.other.dream_world.front_default);
     };
     pokemonImage.addEventListener("click", imageToggle);
     pokemonTitle.innerText = pokemon.name;
-    pokemonsList.append(pokemonElement);
-    pokemonElement.append(pokemonBox);
-    pokemonBox.append(pokemonTitle);
-    pokemonBox.append(pokemonImage);
+    pokemonsList.appendChild(pokemonElement);
+    pokemonElement.appendChild(pokemonBox);
+    pokemonBox.appendChild(pokemonTitle);
+    pokemonBox.appendChild(pokemonImage);
+
+    const pokemonBadgeList = document.createElement("ul");
+    pokemonBadgeList.className = "pokemon-badge-list";
+    for (const type of pokemon.types) {
+      const pokemonBadge = document.createElement("li");
+      pokemonBox.appendChild(pokemonBadgeList);
+      pokemonBadgeList.appendChild(pokemonBadge);
+      pokemonBadge.textContent = type.type.name;
+      for (const color of colours) {
+        pokemonBadge.style.background = color[type.type.name];
+      }
+    }
   });
 };
