31d33dafadb31e345c72d948bd06d546c923e550
angelica <angelica.cavero@gmail.com>
2022-10-18 17:27:17 +0200

    filter added

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const container = document.querySelector(".container");
+const pokemonSearch = document.createElement("input");
+pokemonSearch.type = "search";
+pokemonSearch.placeholder = "Search";
+pokemonSearch.className = "input-search";
+const pokemonsList = document.createElement("ul");
+pokemonsList.className = "pokedex";
+container.append(pokemonSearch);
+container.append(pokemonsList);
+
 const pokemons = fetch("https://pokeapi.co/api/v2/pokemon/")
   .then((response) => response.json())
   .then((data) => {
@@ -8,19 +18,35 @@ const pokemons = fetch("https://pokeapi.co/api/v2/pokemon/")
     });
     return Promise.all(promises);
   })
-  .then((pokemonsListInfo) => {
-    showPokemonsList(pokemonsListInfo);
+  .then((pokemons) => {
+    showPokemonsList(pokemons);
+    setPokemonFilter(pokemons);
   });
 
+const setPokemonFilter = (pokemons) => {
+  pokemonSearch.addEventListener("keyup", (e) => {
+    let searchText = e.target.value;
+    const filteredPokemons = pokemons.filter((poke) => {
+      return poke.name.includes(searchText);
+    });
+    showPokemonsList(filteredPokemons);
+  });
+};
+
 showPokemonsList = (pokemons) => {
-  const pokemonsList = document.querySelector("#pokedex");
+  pokemonsList.innerHTML = "";
   pokemons.forEach((pokemon) => {
     const pokemonElement = document.createElement("li");
     const pokemonBox = document.createElement("div");
     const pokemonTitle = document.createElement("h2");
     const pokemonImage = document.createElement("img");
-
     pokemonImage.setAttribute("src", pokemon.sprites.front_default);
+    const imageToggle = () => {
+      pokemonImage.src === pokemon.sprites.front_default
+        ? (pokemonImage.src = pokemon.sprites.back_default)
+        : (pokemonImage.src = pokemon.sprites.front_default);
+    };
+    pokemonImage.addEventListener("click", imageToggle);
     pokemonTitle.innerText = pokemon.name;
     pokemonsList.append(pokemonElement);
     pokemonElement.append(pokemonBox);
